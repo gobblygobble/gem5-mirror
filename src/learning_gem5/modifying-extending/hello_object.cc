3bfb10f00b161b3b9cec1f1f43a8819af5c7b610
gobblygobble <gobblygobble@kaist.ac.kr>
2020-04-27 14:15:30 +0900

    debugging gem5 finished

--- src/learning_gem5/modifying-extending/hello_object.cc
+++ src/learning_gem5/modifying-extending/hello_object.cc
@@ -1,11 +1,13 @@
 #include "learning_gem5/modifying-extending/hello_object.hh"
+#include "debug/Hello.hh"
 
-#include <iostream>
+//#include <iostream>
 
 HelloObject::HelloObject(HelloObjectParams *params) :
     SimObject(params)
 {
-    std::cout << "Hello World! From a SimObject!" << std::endl;
+    //std::cout << "Hello World! From a SimObject!" << std::endl;
+    DPRINTF(Hello, "Created the hello object\n");
 }
 
 HelloObject*
