acdaf8d48306baa5f0d8dc6b34484cfabc5ec9f8
gobblygobble <gobblygobble@kaist.ac.kr>
2020-05-14 14:23:16 +0900

    added benchmark option for SE mode

--- configs/common/Options.py
+++ configs/common/Options.py
@@ -405,6 +405,8 @@ def addSEOptions(parser):
                            "to be used in syscall emulation."
                            "Usage: gem5.opt [...] --redirects /dir1=/path/"
                            "to/host/dir1 --redirects /dir2=/path/to/host/dir2")
+    parser.add_option("-b", "--benchmark", default="",
+                      help="The benchmark to be loaded.")
 
 
 
