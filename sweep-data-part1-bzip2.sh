5d4f547285063ef5e46bc6eea63e7d4e52038059
gobblygobble <gobblygobble@kaist.ac.kr>
2020-06-24 20:49:03 +0900

    bzip2 data except 4-way1024 and 8-way1024

--- sweep-data-part1-bzip2.sh
+++ sweep-data-part1-bzip2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-for size in 128 256 512 1024
+#for size in 128 256 512 1024
+for size in 256 512 1024
 do
-    for assoc in 2 4 8
+    #for assoc in 2 4 8
+    for assoc in 4 8
     do
         build/X86/gem5.opt configs/SPEC2006/speccpu2006-part1.py --benchmark=bzip2 --cpu-type=MinorCPU --caches --l2dtlb_size=$size --l2dtlb_assoc=$assoc
         mv m5out/stats.txt ./CS510-assignments/final-project/part1/bzip2/$assoc-way-$size-stats.txt
