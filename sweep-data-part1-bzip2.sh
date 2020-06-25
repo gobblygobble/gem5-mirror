39b45efd6d1ead81f0404290f5a72a0c83b48884
gobblygobble <gobblygobble@kaist.ac.kr>
2020-06-25 18:22:48 +0900

    sweep-data script revised

--- sweep-data-part1-bzip2.sh
+++ sweep-data-part1-bzip2.sh
@@ -5,7 +5,7 @@ do
     for assoc in 2 4 8
     do
         build/X86/gem5.opt configs/SPEC2006/speccpu2006-part1.py --benchmark=bzip2 --cpu-type=MinorCPU --caches --l2dtlb_size=$size --l2dtlb_assoc=$assoc
-        mv m5out/stats.txt ./CS510-assignments/final-project/part1/bzip2/$assoc-way-$size-stats.txt
+        mv m5out/stats.txt ./CS510-assignments/final-project/1/bzip2/$assoc-way-$size-stats.txt
     done
 done
 
